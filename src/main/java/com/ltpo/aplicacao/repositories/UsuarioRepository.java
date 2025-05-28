009e69ede1765d7b63c895848b30728cf477ae30
Marcos Vinícius <marcosvini.rg@gmail.com>
2025-05-28 11:02:01 -0300

    Update UsuarioRepository.java

--- src/main/java/com/ltpo/aplicacao/repositories/UsuarioRepository.java
+++ src/main/java/com/ltpo/aplicacao/repositories/UsuarioRepository.java
@@ -3,7 +3,7 @@ package com.ltpo.aplicacao.repositories;
 import com.ltpo.aplicacao.models.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UsuarioRepository extends JpaRepository {
+public interface UsuarioRepository extends JpaRepository <Usuario, Long> {
     Usuario findByEmail(String email);
     boolean existsByEmail(String email);
 }
