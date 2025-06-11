72d051b1608eb8ba4df83d170c3bdf877e73d16e
MarcosVinicius <Marcosvini.rg@gmail.com>
2025-06-11 17:03:58 -0300

    Create CursoService.java

---
+++ src/main/java/com/ltpo/aplicacao/services/CursoService.java
@@ -0,0 +1,38 @@
+package com.ltpo.aplicacao.services;
+
+import com.ltpo.aplicacao.models.Curso;
+import com.ltpo.aplicacao.repositories.CursoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CursoService {
+    @Autowired
+    private CursoRepository cursoRepository;
+
+    public Curso cadastrar(Curso curso){
+        if(cursoRepository.existsByNome((curso.getNome()))){
+            throw new RuntimeException("Curso ja cadastrado");
+        }
+        return cursoRepository.save(curso);
+    }
+
+    public List<Curso> listarTodos(){
+        return cursoRepository.findAll();
+    }
+    public Optional<Curso> findById(Long id){
+        return cursoRepository.findById(id);
+    }
+    public Curso save(Curso curso){
+        return  cursoRepository.save(curso);
+    }
+    public boolean existsById(Long id){
+        return cursoRepository.existsById(id);
+    }
+    public void deleteById(Long id){
+        cursoRepository.deleteById(id);
+    }
+}
