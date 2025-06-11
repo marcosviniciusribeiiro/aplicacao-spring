6f3ee26a2d4b48b81e9f2fea1b786781ac395c0d
Marcos Vinícius <marcosvini.rg@gmail.com>
2025-06-11 11:04:55 -0300

    Update UsuarioController.java

--- src/main/java/com/ltpo/aplicacao/controllers/UsuarioController.java
+++ src/main/java/com/ltpo/aplicacao/controllers/UsuarioController.java
@@ -8,7 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/usuarios")
+@RequestMapping("/api/usuarios")
 public class UsuarioController {
     @Autowired
     private UsuarioService usuarioService;
