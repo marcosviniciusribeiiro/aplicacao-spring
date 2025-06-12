0b003adbaca64f9c1f03069c606a0d66bc84a034
Marcos Vinícius <marcosvini.rg@gmail.com>
2025-06-12 09:55:12 -0300

    Update Usuario.java

--- src/main/java/com/ltpo/aplicacao/models/Usuario.java
+++ src/main/java/com/ltpo/aplicacao/models/Usuario.java
@@ -2,6 +2,9 @@ package com.ltpo.aplicacao.models;
 
 import jakarta.persistence.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 @Table(name = "usuarios")
 public class Usuario {
@@ -17,6 +20,9 @@ public class Usuario {
 
     @Column(nullable = false)
     private String senha;
+
+    @ManyToMany(mappedBy = "cursos")
+    private List<Curso> cursos = new ArrayList<>();
 
     public Long getId() {
         return id;
