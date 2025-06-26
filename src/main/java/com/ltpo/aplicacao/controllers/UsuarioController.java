a1f569524ad7a7723cbe6ffdbabe68e2952426c8
MarcosVinicius <Marcosvini.rg@gmail.com>
2025-06-26 20:24:55 -0300

    Update UsuarioController.java

--- src/main/java/com/ltpo/aplicacao/controllers/UsuarioController.java
+++ src/main/java/com/ltpo/aplicacao/controllers/UsuarioController.java
@@ -1,12 +1,15 @@
 package com.ltpo.aplicacao.controllers;
 
+import com.ltpo.aplicacao.models.Curso;
 import com.ltpo.aplicacao.models.Usuario;
+import com.ltpo.aplicacao.services.CursoService;
 import com.ltpo.aplicacao.services.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -15,8 +18,25 @@ public class UsuarioController {
     @Autowired
     private UsuarioService usuarioService;
 
+    @Autowired
+    private CursoService cursoService;
+
     @PostMapping
     public ResponseEntity<Usuario> cadastrarUsuario(@RequestBody Usuario usuario){
+        List<Curso> cursosProcessados = new ArrayList<>();
+
+        if (usuario.getCursos() != null) {
+            for (Curso curso : usuario.getCursos()) {
+                Curso existente = cursoService.findByNome(curso.getNome());
+                if (existente != null) {
+                    cursosProcessados.add(existente);
+                } else {
+                    cursosProcessados.add(cursoService.save(curso));
+                }
+            }
+            usuario.setCursos(cursosProcessados);
+        }
+
         Usuario novoUsuario = usuarioService.cadastrar(usuario);
         return new ResponseEntity<>(novoUsuario, HttpStatus.CREATED);
     }
@@ -34,13 +54,26 @@ public class UsuarioController {
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity <Usuario> atualizarUsuario(@PathVariable Long id, @RequestBody Usuario dados){
+    public ResponseEntity<Usuario> atualizarUsuario(@PathVariable Long id, @RequestBody Usuario dados){
         return usuarioService.findById(id).map(usuario -> {
             usuario.setNome(dados.getNome());
             usuario.setEmail(dados.getEmail());
             usuario.setSenha(dados.getSenha());
-            usuario.setCursos(dados.getCursos());
-            return  ResponseEntity.ok(usuarioService.save(usuario));
+
+            List<Curso> cursosAtualizados = new ArrayList<>();
+            if (dados.getCursos() != null) {
+                for (Curso curso : dados.getCursos()) {
+                    Curso existente = cursoService.findByNome(curso.getNome());
+                    if (existente != null) {
+                        cursosAtualizados.add(existente);
+                    } else {
+                        cursosAtualizados.add(cursoService.save(curso));
+                    }
+                }
+                usuario.setCursos(cursosAtualizados);
+            }
+
+            return ResponseEntity.ok(usuarioService.save(usuario));
         }).orElse(ResponseEntity.notFound().build());
     }
 
